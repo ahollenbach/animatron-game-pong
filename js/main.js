125d2140e4a8d0b7cadba28af0ba4fd7178d56f5
ahollenbach <hollenbach.andrew@gmail.com>
2013-05-29 14:31:05 -0400

    Updated pong frontend which now displays

--- js/main.js
+++ js/main.js
@@ -8,36 +8,38 @@ window.addEventListener("load", function() {
 	var defaultPos = [0,0];
 
 	//puck attributes
-	var puckPosX 	= 0;
-	var puckPosY 	= 0;
-	var puckRadius 	= 10;
+	var puckPosX 	= WIDTH/2;
+	var puckPosY 	= HEIGHT/2;
+	var puckRadius 	= 14;
 
 	//bar attributes
 	var barWidth 	= 14;
 	var barHeight 	= 80;
 	var barOffset 	= 10; //offset from edge
-	var p1posY 		= 0;
-	var p1posX 		= barOffset;
-	var p2posY 		= 0;
+	var p1posY 		= HEIGHT/2;
+	var p1posX 		= barOffset + barWidth/2;
+	var p2posY 		= HEIGHT/2;
 	var p2posX 		= WIDTH - barOffset - barWidth/2; //all assumes upper right corner as root
 
 	//initialize and begin
-	var scene = b().add(
-						b().circle([puckPosX, puckPosY], puckRadius)
-				  		   .fill('#FFF'))
-				   .add(
-				 		b().rect([p1posX, p1posY], [barWidth,barHeight])
-						   .fill('#FFF'))
-				   .add(
-						b().rect([p2posX, p2posY], [barWidth,barHeight])
-						   .fill('#FFF'));
+	var scene = b('scene')
+					.fill("#000")
+					.add(
+						b('puck').circle([puckPosX, puckPosY], puckRadius)
+				  		   .fill('#000'))
+				    .add(
+				 		b('player1').rect([p1posX, p1posY], [barWidth,barHeight])
+						   .fill('#000'))
+				    .add(
+						b('player2').rect([p2posX, p2posY], [barWidth,barHeight])
+						   .fill('#000'));
 
-	createPlayer('game-canvas', { 
+	createPlayer('game-canvas', {
 		"mode" : C.M_DYNAMIC,
 		"anim" : {
 			"width" : WIDTH,
 			"height" : HEIGHT,
-			"bgfill" : { color : "#000" }
+			"bgfill" : { color : "#FFF" }
 		} 
 	})
 		.load(scene)
