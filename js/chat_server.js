47e4d85a6485bb26afdfb541407f031cf14565c1
Brian Clanton <brian.clanton@gmail.com>
2013-05-31 10:39:29 -0400

    Added comments to chat_server.js.

--- js/chat_server.js
+++ js/chat_server.js
@@ -1,3 +1,4 @@
+// Possible message types that can be received from clients
 const MessageTypes = {
     INITIAL : "initial",
     MESSAGE : "message",
@@ -6,10 +7,20 @@ const MessageTypes = {
     START_PONG : "start_pong"
 }
 
+// Regular expression used to check username validity. The rules are as follows:
+// Usernames can consist of lowercase and capitals
+// Usernames can consist of alphanumeric characters
+// Usernames can consist of underscore and hyphens and spaces
+// Cannot be two underscores, two hypens or two spaces in a row
+// Cannot have a underscore, hypen or space at the start or end
 var usernameValidator = /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/;
 
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 1337});
+// Websocket initialization
+var WebSocket = require('ws');
+var WebSocketServer = WebSocket.Server;
+
+// Open server on port 1337
+var wss = new WebSocketServer({port: 1337});
 
 console.log("Server started");
 
@@ -22,7 +33,9 @@ wss.on('connection', function(ws) {
     //ws.send('New user in chat');
     logClients();
 
+    // Event handler for incoming messages
     ws.on('message', function(message) {
+        // Verify that message is JSON
         try {
             var json = JSON.parse(message);
         } catch (e) {
@@ -31,17 +44,21 @@ wss.on('connection', function(ws) {
         }
 
         switch (json.type) {
+            // Initial message from client, which sends user data such as username
             case MessageTypes.INITIAL:
+                // Connect to chat if the username is valid, or else report invalid username and refuse connection
                 if (usernameValidator.test(json.data.username)) {
                     username = json.data.username;
 
+                    // Send connection success message
                     ws.send(JSON.stringify({
                         type : "connection_success",
                         data : {
-                            message : "Welcome to the chat."
+                            message : "Welcome to the chat " + username;
                         }
                     }));
                 } else {
+                    // Send connection failure message
                     ws.send(JSON.stringify({
                         type : "connection_failure",
                         data : {
@@ -51,7 +68,9 @@ wss.on('connection', function(ws) {
                 }               
                 break;
 
+            // Chat message
             case MessageTypes.MESSAGE:
+                // Broadcast chat message to all users
                 for (var i = 0; i < clients.length; i++)
                     sendMessage(clients[i], i == index ? "Me" : username, json.data.message);
                 break;
@@ -62,21 +81,25 @@ wss.on('connection', function(ws) {
         }          
     });
 
+    // Event handler for closed connection between server and a client
     ws.on('close', function(connection) {
     	console.log("A user disconnected");
 
+        // Remove disconnected client from list of clients
     	clients.splice(index, 1);
     	
     	logClients();
     });
 });
 
+// Logs current number of clients to command line
 function logClients() {
 	console.log("There are currently " + clients.length + " clients online.");
 }
 
+// Sends message text along with other user information as JSON to a client
 function sendMessage(client, username, message) {
-	if (client) {
+	if (client.readyState == WebSocket.OPEN) {
         client.send(JSON.stringify({
             type : "message",
             data : {
